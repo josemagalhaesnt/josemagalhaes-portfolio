a3605afa725616d1ff21a5563eef144ed61ba4e0
José Magalhães <zneto@live.com>
2020-06-24 02:52:02 -0300

    Header + Light and Dark Theme

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,10 @@
+export default {
+    title: 'dark',
+    colors: {
+        primary: '#1B1B3A',
+        secondary: '#693668',
+
+        background: '#222',
+        text: '#fff',
+    }
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,10 @@
+export default {
+    title: 'light',
+    colors: {
+        primary: '#4BB3FD',
+        secondary: '#0496FF',
+
+        background: '#f5f5f5',
+        text: '#333',
+    }
+};
